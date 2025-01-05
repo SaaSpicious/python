6d0976a06b08fb005d4b1e9d3c7fe72acdb40174
bf-dennis <Dennis.Klein87@gmail.com>
2025-01-05 22:14:43 +0100

    Day 29: Simplify Password Generation mechanism

--- challenge/29_password_generator/main.py
+++ challenge/29_password_generator/main.py
@@ -8,34 +8,22 @@ MOST_USED_USERNAME = "Dennis.Klein87@gmail.com"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    password_easy=[]
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    nr_letters = random.randint(8,12)
-    nr_symbols = random.randint(4,6)
-    nr_numbers = random.randint(4,6)
+    password_letters = [random.choice(letters) for _ in range(random.randint(8,12))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(4, 6))]
+    password_numbers = [random.choice(numbers) for _ in range(random.randint(4, 6))]
 
-    for n in range(1,nr_letters+1):
-        password_easy.append(random.choice(source.letters))
-
-    for n in range(1,nr_symbols+1):
-        password_easy.append(random.choice(source.symbols))
-
-    for n in range(1,nr_numbers+1):
-        password_easy.append(random.choice(source.numbers))
-
-    password_easy_string=""
-    for character in password_easy:
-        password_easy_string+=character
-
-    password_hard=password_easy
-    random.shuffle(password_hard)
-
-    password_hard_string=""
-    for character in password_hard:
-        password_hard_string+=character
+    password = password_letters + password_symbols + password_numbers
+    random.shuffle(password)
+    password = "".join(password)
 
     password_input.delete(0, END)
-    password_input.insert(index=0, string=password_hard_string)
+    password_input.insert(index=0, string=password)
     return True
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
