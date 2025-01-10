8861d3a510e2ceb2388393fadd59b3ee20848daf
bf-dennis <Dennis.Klein87@gmail.com>
2025-01-10 21:04:20 +0100

    Add error handling to NATO alphabeth project

--- challenge/26_nato_alphabeth/main.py
+++ challenge/26_nato_alphabeth/main.py
@@ -2,9 +2,14 @@ import pandas
 
 alphabet = pandas.read_csv('nato_phonetic_alphabet.csv')
 
-name = input("Which name do you need to have spelled? ").upper()
+def get_phonetic():
+    name = input("Which name do you need to have spelled? ").upper()
+    try:
+        spelling = [alphabet[alphabet.letter == n].code.item() for n in name]
+    except ValueError:
+        print("Sorry, only letters allowed in here!")
+        get_phonetic()
+    else:
+        print(spelling)
 
-spelling = [alphabet[alphabet.letter == n].code.item() for n in name]
-
-print(spelling)
-
+get_phonetic()
